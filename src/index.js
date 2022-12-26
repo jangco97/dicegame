9ec0ff3603b142b2db30cbc3031b5f766ab2feba
geon woo <qkrnslehd@naver.com>
2022-12-26 18:50:44 +0900

    주사위 컴포넌트 및 App컴포넌트 작성

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>주사위 게임</h1>);
+
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부';
+}
+function handleClick(){
+  alert("가위바위보!")
+}
+const me = 'rock'
+const other = 'scissor'
+const result = getResult(me, other)
+root.render(
+  <>
+    <h1 idFor="title">가위바위보</h1>
+    <App></App>
+    <h2>
+      <p>{result}</p>
+    </h2>
+    <button onClick={handleClick}>가위</button>
+    <button onClick={handleClick}>바위</button>
+    <button onClick={handleClick}>보</button>
+  </>
+
+);
 
