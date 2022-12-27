71eb7b98c312607af9e4084a0bbc8b8d743cbedb
geon woo <qkrnslehd@naver.com>
2022-12-27 13:02:33 +0900

    총점 구하기, 나온 숫자 기록하기

--- src/App.js
+++ src/App.js
@@ -8,12 +8,26 @@ function random(n){
 
 
 function App() {
+    //useState를 써서 해당 변수의 값을 효율적으로 바꿀 수 있음
     const [num, setNum] = useState(1)
+    const [sum, setSum] = useState(0)
+    const [gameHistory, setGameHistory] = useState([])
+    console.log(gameHistory)
     const handleRollClick = () => {
-        setNum(random(6))
+        const nextNum = random(6)
+        setNum(nextNum)
+        setSum(sum + nextNum)
+        console.log(typeof gameHistory)
+        //배열의 경우 참조형이기 때문에 배열에 값을 넣어도 주소값이 변하지 않는다.
+        //useState는 주소값이 변해야 값이 변했다고 인지하고 재렌더링을 하기 때문에 아래와 같이 spread를 이용해 
+        //새로운 배열에 값을 할당해야함
+        setGameHistory([...gameHistory, nextNum])
+        console.log(gameHistory)
     }
     const handleClearClick = () => {
         setNum(1)
+        setSum(0)
+        setGameHistory([])
     }
     return (
         <div>
@@ -21,7 +35,12 @@ function App() {
                 <Button onClick={handleRollClick}>던지기</Button>
                 <Button onClick={handleClearClick}>처음부터</Button>
             </div>
-            <Dice color="red" num ={num}></Dice>
+            <h2>나</h2>
+            <Dice color="red" num = {num}></Dice>
+            <h2>총점</h2>
+            <p>{sum}</p>
+            <h2>기록</h2>
+            <p></p>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,33 +3,11 @@ import React from 'react';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const WINS = {
-  rock: 'scissor',
-  scissor: 'paper',
-  paper: 'rock',
-};
 
-function getResult(left, right) {
-  if (WINS[left] === right) return '승리';
-  else if (left === WINS[right]) return '패배';
-  return '무승부';
-}
-function handleClick(){
-  alert("가위바위보!")
-}
-const me = 'rock'
-const other = 'scissor'
-const result = getResult(me, other)
+
 root.render(
   <>
-    <h1 id="title">가위바위보</h1>
     <App></App>
-    <h2>
-      <p>{result}</p>
-    </h2>
-    <button onClick={handleClick}>가위</button>
-    <button onClick={handleClick}>바위</button>
-    <button onClick={handleClick}>보</button>
   </>
 
 );
